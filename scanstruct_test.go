157872bb90dc93e1a6701ed1f9e6c44e38af6f66
lodmev <lodmev@gmail.com>
2022-02-15 23:52:50 +0300

    Update and rename sqlstruct_test.go to scanstruct_test.go

--- scanstruct_test.go
+++ scanstruct_test.go
@@ -1,7 +1,7 @@
 // Copyright 2012 Kamil Kisiel. All rights reserved.
 // Use of this source code is governed by the MIT
 // license which can be found in the LICENSE file.
-package sqlstruct
+package scanstruct
 
 import (
 	"reflect"
